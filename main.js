c471ce5607ec6afc7c08edddae3d7214898be282
AnnumNazir <annumnazir@gmail.com>
2024-03-20 23:31:33 +0500

    published on 20-03-2023

---
+++ main.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    {
+        message: "enter second Number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "select one operator from operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "subtraction", "Multiplication", "Division"],
+    },
+]);
+// console.log(answer);
+if (answer.operator === "Addition") {
+    console.log("your answer is", answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log("your answer is", answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log("your answer is", answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log("your answer is", answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please enter valid operator");
+}
